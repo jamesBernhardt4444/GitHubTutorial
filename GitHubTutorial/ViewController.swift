3f6050a067dc48d2e6756120b73ff6b75d0a2a0c
James Bernhardt <james.bernhardt3@gmail.com>
2018-03-13 07:53:26 +0000

    Change button back ground colour and text. New print statements.

--- GitHubTutorial/ViewController.swift
+++ GitHubTutorial/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let name = "James"
+        if name == "James" {
+            print("Hello \(name)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
